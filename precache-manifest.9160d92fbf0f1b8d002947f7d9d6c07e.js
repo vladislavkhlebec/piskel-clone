d50d8d8facdcf943a3c49e2a5a05769df91bdc72
Vladislav Khlebec <vladislavkhlebec@gmail.com>
2019-07-13 12:03:18 +0300

    Updates

---
+++ precache-manifest.9160d92fbf0f1b8d002947f7d9d6c07e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6ea32f303d989b347de162ce145e1dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc7281dc67b3d90487ca",
+    "url": "/static/css/main.af1754a2.chunk.css"
+  },
+  {
+    "revision": "c6e4e7f51bb9703b6e7d",
+    "url": "/static/js/2.9e72ecb9.chunk.js"
+  },
+  {
+    "revision": "cc7281dc67b3d90487ca",
+    "url": "/static/js/main.c8362198.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "580ffd26ccaa16481ed87c1829c74943",
+    "url": "/static/media/1200px-React-icon.svg.580ffd26.png"
+  },
+  {
+    "revision": "ddb99b16f9084a54f224ce047c9ebfcd",
+    "url": "/static/media/200x200_7V0.ddb99b16.gif"
+  },
+  {
+    "revision": "402b3baaeb428992fac736cbb06d57bd",
+    "url": "/static/media/200x200_W5Jq.402b3baa.gif"
+  },
+  {
+    "revision": "c99e4ce7368fd074a0c3572baa03b2aa",
+    "url": "/static/media/214x200_x8.c99e4ce7.gif"
+  },
+  {
+    "revision": "f1aa5bd26110ede5506ffaf1545fa27e",
+    "url": "/static/media/3Mf6.f1aa5bd2.gif"
+  },
+  {
+    "revision": "2006a7fb0d51603417f5cac4e7ec8e41",
+    "url": "/static/media/5EDq.2006a7fb.gif"
+  },
+  {
+    "revision": "2ef4b33ebb3b23f0cf4bedec21ef1759",
+    "url": "/static/media/ForAbout.2ef4b33e.jpg"
+  },
+  {
+    "revision": "40cb707f4cf722302891471e0c86409c",
+    "url": "/static/media/VIR.40cb707f.gif"
+  },
+  {
+    "revision": "939c525bb4a302febc8945db132d2b7a",
+    "url": "/static/media/images.939c525b.jpg"
+  },
+  {
+    "revision": "73437451a40826dc69b178df4ce89325",
+    "url": "/static/media/pencil.73437451.png"
+  },
+  {
+    "revision": "d39379c62510648ed900f8604414cc4f",
+    "url": "/static/media/preview.d39379c6.gif"
+  }
+]);
